4a7cf27ab499558918cffb8b7184c4926fdb2e1d
ohtaehyun <tmzkflsk118@naver.com>
2017-11-24 18:16:24 +0900

    60142304 오태현 수정파일

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,13 @@
 from tkinter import *
+def finish(player):
+      return((list[0]['text']==list[1]['text']==list[2]['text']==player)or
+             (list[3]['text']==list[4]['text']==list[5]['text']==player)or
+             (list[6]['text']==list[7]['text']==list[8]['text']==player)or
+             (list[0]['text']==list[3]['text']==list[6]['text']==player)or
+             (list[1]['text']==list[4]['text']==list[7]['text']==player)or
+             (list[2]['text']==list[5]['text']==list[8]['text']==player)or
+             (list[0]['text']==list[4]['text']==list[8]['text']==player)or
+             (list[2]['text']==list[4]['text']==list[6]['text']==player))
 
 def checked(i) :
       global player
@@ -12,9 +21,15 @@ def checked(i) :
       if player == "X" :
             player = "O"
             button["bg"] = "yellow"
+            if finish("X")==True:
+                  print("X win")
+                  window.destroy()
       else :
             player = "X"
             button["bg"] = "lightgreen"
+            if finish("O")==True:
+                  print("O win")
+                  window.destroy()
 
 window = Tk()
 player = "X"
